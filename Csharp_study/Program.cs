89df5261dbe577bb3f150814eb852cabe4eb564a
user <user@DESKTOP-2QJCBCK>
2022-04-14 11:04:19 +0900

    초기 커밋.

---
+++ Csharp_study/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "사과", "바나나", "석류", "참외" };
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
